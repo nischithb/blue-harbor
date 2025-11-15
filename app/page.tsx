e11725dde1a1943c9e924c9c70f94855bf6fd17b
Nischith <nischithb01@gmail.com>
2025-11-15 15:49:39 +0530

    chore: init nextjs project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="h-scre">
+      <h1>Welcome to Blue Harbor</h1>
+      <p>Your one-stop solution for launching and showcasing your next big idea.</p>
+    </div>
+  );
+}
