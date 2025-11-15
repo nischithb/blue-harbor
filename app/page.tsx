636e6f621a145038c83999b180a780d540a3c9a2
Nischith <nischithb01@gmail.com>
2025-11-15 16:16:10 +0530

    chore: setup formatter

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="h-scre">
+    <div className="h-screen flex flex-col items-center justify-center">
       <h1>Welcome to Blue Harbor</h1>
       <p>Your one-stop solution for launching and showcasing your next big idea.</p>
     </div>
